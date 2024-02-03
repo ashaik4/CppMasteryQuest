cfeb2163a757fb6374f9cbe9e07008f91b34b69a
Arshad Shaik <arshad.shaik@tradingtechnologies.com>
2024-02-03 21:23:58 +0530

    Makefile clean up and more testing of Account class

--- chap03/Account/src/AccountTest.cpp
+++ chap03/Account/src/AccountTest.cpp
@@ -14,10 +14,16 @@ int main(){
     // Default constructor is only provided if no constructor is defined.
 
     // Create more Account objects that use both `name` and `balance`
-    Account account3{"John Appleseed", 50};
+    Account account3{"John Appleseed", 200};
     Account account4{"Sony Ericsson", 100};
     std::cout << "account3 name is: " << account3.getName() <<"\t" << "Balance is: "<< account3.getBalance() << std::endl;
     std::cout << "account4 name is: " << account4.getName() <<"\t" << "Balance is: "<< account4.getBalance() << std::endl;
+
+    // Deposit and withdraw from account3
+    account3.deposit(50);
+    std::cout<< "After depositing 50, account3 balance is: " << account3.getBalance() << std::endl;
+    account3.withdraw(100);
+    std::cout<< "After withdrawing 100, account3 balance is: " << account3.getBalance() << std::endl;
 
 
     
